0982ddd2461fc8efe6a82e6990bebf997de04f8c
laasyagudisa <laasyagudisa@gmail.com>
2024-02-24 15:45:10 -0600

    experimenting on sponsor design

--- web.js
+++ web.js
@@ -67,37 +67,7 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 //================================================sponsor JS==========================================================
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    const maxScroll = slidesContainer.scrollWidth - slidesContainer.clientWidth + 1;
-
-    // Check if next slide is beyond the last slide
-    if (slidesContainer.scrollLeft + slideWidth >= maxScroll) {
-        // Reset to the first slide
-        slidesContainer.scrollLeft = 0;
-    } else {
-        // Scroll to the next slide
-        slidesContainer.scrollLeft += slideWidth;
-    }
-});
-
-prevButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-
-    // Check if previous slide is beyond the first slide
-    if (slidesContainer.scrollLeft - slideWidth < 0) {
-        // Reset to the last slide
-        slidesContainer.scrollLeft = slidesContainer.scrollWidth - slidesContainer.clientWidth;
-    } else {
-        // Scroll to the previous slide
-        slidesContainer.scrollLeft -= slideWidth;
-    }
-});
 //================================================FAQ JS==========================================================
 document.addEventListener("DOMContentLoaded", function () {
     var accordions = document.getElementsByClassName("accordion");
